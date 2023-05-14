a52f38048e0ae5da158759b0973fe75051437a17
Mít <hoanglonglpgxxx@gmail.com>
2023-05-15 00:24:34 +0700

    almost done

--- lib/b4.dart
+++ lib/b4.dart
@@ -2,103 +2,60 @@ import 'package:flutter/material.dart';
 
 class MyApp4 extends StatelessWidget {
   const MyApp4({super.key});
-
-  //Statefulwidget has internal state
-  //each StatefulWidget has 2 class
-  //* a public class, that extends StatefulWidget
-  //* a private class, which is bluid()
-
-/*   String name = '';
-  int age = 0;
-
-  MyApp4({required this.name, required this.age}); */
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Multiplation',
       home: Scaffold(
-        body: Center(
-            child: Container(
-                decoration: const BoxDecoration(), child: MultiTable())),
-      ),
-    );
-  }
-}
-
-class MultiTable extends StatelessWidget {
-  int index = 9;
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> table = [];
-
-    for (var i = 1; i <= index; i++) {
-      List<Widget> row = [];
-      for (var k = 1; k <= index; k++) {
-        row.add(Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Container(
-            child: Text(
-              '$i * $k = ${i * k} ',
-              style: TextStyle(fontSize: 20),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Multiplation Table'.toUpperCase(),
+              style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.cyan,
+                  fontWeight: FontWeight.bold),
             ),
-          ),
-        ));
-
-        //row.add(Text(
-        //'$k * $i = ${i * k} ',
-        //style: TextStyle(fontSize: 8),
-        //));
-      }
-
-      table.add(Column(children: row));
-    }
-
-    return Container(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: table,
-          ),
-        ]));
-  }
-} 
-/* class MultiTable extends StatelessWidget {
-  int index = 10;
-
-  MultiTable({super.key});
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> table = [];
-    for (int i = 1; i < index; i++) {
-      List<Widget> row = [];
-
-      for (int j = 1; j <= i; j++) {
-        row.add(Container(
-            child: Container(
-          child: Text('$i * $j = ${i * j}'),
-        )));
-      }
-      table.add(Column(children: row));
-    }
-
-    /* return MaterialApp(
-      title: 'statefulWidget',
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: table,
-          ),
+            Container(
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical, child: _MultiTable()),
+              //SingleChildScrollView nhưng không scroll?
+            )
+          ],
         ),
       ),
-    ); */
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-      child: Column(
-        children: table,
-      ),
     );
   }
 }
- */
+
+class _MultiTable extends StatelessWidget {
+  int index = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> table = [];
+    for (int i = 1; i <= index; i++) {
+      List<Widget> row = [];
+      for (int j = 1; j <= index; j++) {
+        row.add(Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Container(
+              child: Text(
+                '$i * $j = ${i * j} ',
+                style: const TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )));
+      }
+      table.add(Column(children: row)); //TODO:  đủ 9 row -> add
+    }
+    return Container(
+        child: Row(
+      //TODO:  row các cột
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: table,
+    ));
+  }
+}
--- lib/test.dart
+++ lib/test.dart
@@ -1,102 +1,34 @@
-import 'package:flutter/material.dart';
-
-class MyApp4 extends StatelessWidget {
-  const MyApp4({super.key});
-
-  //Statefulwidget has internal state
-  //each StatefulWidget has 2 class
-  //* a public class, that extends StatefulWidget
-  //* a private class, which is bluid()
-
-/*   String name = '';
-  int age = 0;
-
-  MyApp4({required this.name, required this.age}); */
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Container(
-                decoration: const BoxDecoration(), child: MultiTable())),
-      ),
-    );
-  }
-}
-
 /* class MultiTable extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> res = [];
-
-    for (var i = 1; i <= 9; i++) {
-      List<Widget> row = [];
-      for (var k = 1; k <= i; k++) {
-        row.add(Container(
-          child: SizedBox(
-            child: Text(
-              '$k * $i = ${i * k} ',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          decoration: BoxDecoration(border: Border.all(color: Colors.green)),
-        ));
-
-        //row.add(Text(
-        //'$k * $i = ${i * k} ',
-        //style: TextStyle(fontSize: 8),
-        //));
-
-        row.add(SizedBox(
-          width: 10,
-        ));
-      }
-
-      res.add(Row(children: row));
-      res.add(SizedBox(
-        height: 20,
-      ));
-    }
-
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-      child: Column(
-        children: res,
-      ),
-    );
-  }
-} */
-class MultiTable extends StatelessWidget {
   int index = 10;
-
-  MultiTable({super.key});
   @override
   Widget build(BuildContext context) {
     List<Widget> table = [];
-    for (int i = 1; i < index; i++) {
-      for (int j = 1; j < index; j++) {
-        table.add(Table(
-          children: [
-            TableRow(children: [
-              Container(
-                child: Text('$i * $j = ${i * j}'),
-              )
-            ])
-          ],
+
+    for (var i = 1; i <= index - 1; i++) {
+      List<Widget> row = [];
+      for (var j = 1; j <= index; j++) {
+        row.add(Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Container(
+            child: Text(
+              '$i * $j = ${i * j} ',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
         ));
       }
+
+      table.add(Column(children: row));
     }
 
-    return MaterialApp(
-      title: 'statefulWidget',
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: table,
           ),
-        ),
-      ),
-    );
+        ]));
   }
-}
+}  */
