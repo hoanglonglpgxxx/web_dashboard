225916e75cbb6b30bcfc328ed5007da21ba2494b
Mits <hoanglonglpgxxx@gmail.com>
2023-05-25 16:54:37 +0700

    up

--- lib/stream_async_await.dart
+++ lib/stream_async_await.dart
@@ -10,9 +10,24 @@ extension Run on Cat {
   }
 }
 
+/**
+ * future: data to be returned in the future
+ */
 
-void main() {
+Future<int> multipleByTwo(int a) {
+  return Future.delayed(Duration(seconds: 3), () => a * 2);
+}
+
+extension Run2 on Cat {
+  //use a getter
+  String get catName => 'cat name is $name';
+}
+
+void main() async {
+  final res = await multipleByTwo(20);
+  print(res);
   final cat = Cat('a');
   cat.run();
   print(cat.name);
+  print(cat.catName);
 }
