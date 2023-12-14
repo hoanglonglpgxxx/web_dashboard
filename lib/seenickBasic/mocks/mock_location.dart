e13c617104b1f280020735e239b2e343742d8f8a
Mits <hoanglonglpgxxx@gmail.com>
2023-12-14 18:13:37 +0700

    up

--- lib/seenickBasic/mocks/mock_location.dart
+++ lib/seenickBasic/mocks/mock_location.dart
@@ -1,1 +1,15 @@
-import 'package:mocking/main.dart';
+import 'package:web_dashboard/seenickBasic/models/location_fact.dart';
+
+import '../models/location.dart';
+
+class MockLocation extends Location {
+  static FetchAny() {
+    return Location(name: 'test', url: 'test2', facts: <LocationFact>[
+      LocationFact(
+        title: 'fact1',
+        text: 'text1',
+      ),
+      LocationFact(title: 'fact2', text: 'text2')
+    ]);
+  }
+}
