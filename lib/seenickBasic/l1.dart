b0737553608916f17939f9ca93f49565cceb29b9
Mits <hoanglonglpgxxx@gmail.com>
2023-12-20 17:39:31 +0700

    up

--- lib/seenickBasic/l1.dart
+++ lib/seenickBasic/l1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/location.dart';
+import 'package:flutter/foundation.dart';
 
 class fLession extends StatelessWidget {
   final Location location;
@@ -13,11 +14,19 @@ class fLession extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderFacts(location),
+          children: _renderBody(context, location),
         ));
   }
 
-  List<Widget> _renderFacts(Location location) {
+  List<Widget> _renderBody(BuildContext context, Location location) {
+    var result = <Widget>[];
+    result.add(_bannerImage(location.url, 170.0));
+    result.addAll(_renderFacts(context, location));
+    debugPrint('result: $result');
+    return result;
+  }
+
+  List<Widget> _renderFacts(BuildContext context, Location location) {
     var result = <Widget>[];
     for (int i = 0; i < location.facts.length; i++) {
       result.add(_sectionTitle(location.facts[i].title));
@@ -33,6 +42,13 @@ class fLession extends StatelessWidget {
   Widget _sectionTitle(String title) {
     return Text(title);
   }
+
+  Widget _bannerImage(String url, double height) {
+    return Container(
+      constraints: BoxConstraints.tightFor(height: height),
+      child: Image.network(url, fit: BoxFit.fitWidth),
+    );
+  }
 }
 
 
@@ -46,3 +62,7 @@ class fLession extends StatelessWidget {
 //You can replace "String" with any other data type to create a list of that type. The List class provides various methods for adding, removing, and manipulating elements in the list.
 
 //location_fact is constructor to get text and title, then location is contructor to get name, url and an array that is location_fact, mock_location pass an Location_type array that contains values
+
+//working with imgs: dùng Image.netork()
+//.addAll(): add tất cả val vào sau cùng mảng
+//log widget dùng debugPrint
